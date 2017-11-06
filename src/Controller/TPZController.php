f8a25d2f511fbb52f35915c976ade6d43073ebd8
alix.devynck <devynckalix@gmail.com>
2017-11-06 14:41:38 +0100

    Premier TP

--- src/Controller/TPZController.php
+++ src/Controller/TPZController.php
@@ -7,12 +7,17 @@
  */
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-class HelloController{
+class TPZController extends Controller{
     function index()
     {
 
+        return $this->render(
+            'Hello/Hello.html.twig'
+
+        );
     }
 }
